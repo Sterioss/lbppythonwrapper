dac896cca5c67b3d49748831aa9dd6e0db05da72
thomas.tosch <thomas.tosch@uha.fr>
2021-12-01 20:14:36 +0100

    push

--- pylbp.py
+++ pylbp.py
@@ -9,7 +9,7 @@ prgm = os.path.join(dir_path ,"clbp")
 
 def lbp_hist(img_path, color):
     proc = subprocess.Popen([prgm, img_path, color], stdout=subprocess.PIPE)
-    img_hist = proc.stdout.readline()
+    for x in range(3):
+        img_hist[x] = proc.stdout.readline().decode("utf-8").split(',')
     proc.wait()
-    print(img_hist)
-    #return img_hist.decode("utf-8").split(" | ").split(',')
+    return img_hist
