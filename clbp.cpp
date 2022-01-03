783094cd329cc5e66631668a28bf59f19f75a0a6
thomas.tosch <thomas.tosch@uha.fr>
2022-01-03 15:25:16 +0100

    changes
    Signed-off-by: thomas.tosch <thomas.tosch@uha.fr>

--- clbp.cpp
+++ clbp.cpp
@@ -35,9 +35,8 @@ int main(int argc, char **argv) {
     {
         for(int x = 0; x < img.rows; x += img.rows / 8) {
             memset(histogramme, 0, sizeof(histogramme));
-            cout << img.cols << ' ' << img.rows << endl;
-            //Mat sub = img(cv::Rect(x, y, (img.cols / 8), (img.rows / 8))).clone();
-            //img_2_lbp_hist(sub, histogramme);
+            Mat sub = img(cv::Rect(x, y, (img.cols / 8), (img.rows / 8))).clone();
+            img_2_lbp_hist(sub, histogramme);
             for (auto & i : histogramme) {
                 finalHistogramme[idx] = i[0];
                 idx++;
