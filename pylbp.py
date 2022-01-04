c984b188f14d86fe3d700c25d54bc461ce3e48f7
thomas.tosch <thomas.tosch@uha.fr>
2022-01-04 14:32:11 +0100

    changes
    Signed-off-by: thomas.tosch <thomas.tosch@uha.fr>

--- pylbp.py
+++ pylbp.py
@@ -9,7 +9,7 @@ prgm = os.path.join(dir_path ,"clbp")
 
 def lbp_hist(img_path, color):
     proc = subprocess.Popen([prgm, img_path, color], stdout=subprocess.PIPE)
-    line = proc.stdout.readline()[:-2]
+    line = proc.stdout.readline()
     output = line.decode("utf-8").split(',')
     proc.wait()
     return output
